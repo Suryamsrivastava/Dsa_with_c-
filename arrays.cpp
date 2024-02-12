6d9bfd429609291684815e724c11a3a3599f0ebe
Suryamsrivastava <159825860+Suryamsrivastava@users.noreply.github.com>
2024-02-13 01:14:26 +0530

    add file

---
+++ arrays.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int marks[4] = {23,54,48,60};
+// cout<<marks[0]<<endl;
+// cout<<marks[1]<<endl;
+// cout<<marks[2]<<endl;
+// cout<<marks[3]<<endl;
+
+// array with for loop
+// marks[2] = 455;
+// for(int i = 0; i<4; i++)
+
+// cout<<"the value of marks "<< i <<" is "<<marks[i]<<endl;
+
+// array with while loop;
+
+// 0.
+
+// array with do while loop
+
+int i = 0;
+do{
+cout<<"the value of "<< i <<" is "<< marks[i]<<endl;
+i++;
+} while(i<4);
+
+    return 0;
+} 
